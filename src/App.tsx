6245aef8e6cde8320af40fdcde2535bda4fa8fc8
filyys <filipsysak@gmail.com>
2024-08-21 17:24:37 +0200

    Initial first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center bg-background text-foreground">
+        <h1 className="text-3xl font-bold">
+          Personal github template using Vite, React, Typescript, TailwindCSS
+          and ShadcnUI
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
